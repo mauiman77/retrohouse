a04a4fe963b7f1dbf0b12c01495cd71631c41c7b
mauiman77 <boerbasher@protonmail.com>
2023-04-03 21:41:15 +0100

    Create mosaic grid portfolio

---
+++ script.js
@@ -0,0 +1,10 @@
+const toAppend = document.querySelector('.portfolio-grid')
+
+
+for (let x = 0; x < 40; x++) {
+    const thumb = document.createElement('div');
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    thumb.setAttribute('class', 'thumbnail');
+    thumb.style.cssText = `background-color: #${randomColor}`
+    toAppend.appendChild(thumb);
+}
