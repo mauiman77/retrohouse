f5b8cadc224075f57a961ea6bc5fd44f13a0f96e
mauiman77 <boerbasher@protonmail.com>
2023-04-12 16:31:42 +0100

    Create typeScript branch

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-const toAppend = document.querySelector('.portfolio-grid')
-
+const toAppend = document.querySelector(".portfolio-grid");
 
 for (let x = 0; x < 40; x++) {
-    const thumb = document.createElement('div');
-    var randomColor = Math.floor(Math.random()*16777215).toString(16);
-    thumb.setAttribute('class', 'thumbnail');
-    thumb.style.cssText = `background-color: #${randomColor}`
-    toAppend.appendChild(thumb);
+  const thumb = document.createElement("div");
+  var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  thumb.setAttribute("class", "thumbnail");
+  thumb.style.cssText = `background-color: #${randomColor}`;
+  toAppend.appendChild(thumb);
 }
